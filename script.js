d6cf97a8b494fb0f02c3c30281e111dca7eb6d2c
BehzadRG1 <87753809+BehzadRG1@users.noreply.github.com>
2022-02-11 01:38:20 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function load() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.github.com/users", true);
+
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            var users = JSON.parse(this.responseText);
+
+            for (var i in users) {
+
+                var li = document.createElement("li");
+                li.className = "list-group-item user-item text-left";
+
+                var img = document.createElement("img");
+                img.className = "img-circle img-user img-thumbnail";
+                img.setAttribute("src", users[i].avatar_url);
+
+                li.append(img);
+
+                var h3 = document.createElement("h3");
+                var a = document.createElement("a");
+                a.innerText = users[i].login;
+                h3.append(a);
+
+                img.after(h3);
+
+                var ul = document.querySelector(".list-group");
+                ul.append(li);
+
+
+            }
+
+        }
+    }
+
+
+    xhr.send();
+
+}
+
+load();
